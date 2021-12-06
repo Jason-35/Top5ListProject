65a675482dc99d94dac972b37d57fff084156ff4
Jason Wu <jason.wu.2@stonybrook.edu>
2021-12-06 18:38:03 -0500

    style community card and like features

--- client/src/components/CommunityListCard.js
+++ client/src/components/CommunityListCard.js
@@ -9,6 +9,8 @@ import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import dateFormat from "dateformat";
+import Link from "@mui/material/Link";
 import api from "../api";
 
 function CommunityListCard(props) {
@@ -16,6 +18,17 @@ function CommunityListCard(props) {
   const { auth } = useContext(AuthContext);
   const { idNamePair } = props;
   const [expanding, setExpanding] = useState(false);
+
+  let likeOffset = 0;
+  if (idNamePair.likes > 0) {
+    likeOffset = 1;
+  }
+  let dislikeOffset = 0;
+  if (idNamePair.dislikes > 0) {
+    dislikeOffset = 1;
+  }
+  const [like, setLike] = useState(idNamePair.likes + likeOffset);
+  const [dislike, setDislike] = useState(idNamePair.likes + dislikeOffset);
 
   function handleLoadList(event, id) {
     event.stopPropagation();
@@ -29,14 +42,16 @@ function CommunityListCard(props) {
     setExpanding(!expanding);
   }
 
-  function like(event, id) {
+  function handlelike(event, id) {
     event.stopPropagation();
-    store.likeList(id);
+    store.likeList(id, like);
+    setLike(like + 1);
   }
 
-  function dislike(event, id) {
+  function handledislike(event, id) {
     event.stopPropagation();
-    store.communitydislikeList(id);
+    store.dislikeList(id, dislike);
+    setDislike(dislike + 1);
   }
 
   let item1 = " ";
@@ -53,12 +68,16 @@ function CommunityListCard(props) {
   }
 
   let cardElement = (
-    <div>
-      <div>
+    <div
+      style={{
+        margin: "15px",
+      }}
+    >
+      <Box>
         <ListItem
           id={idNamePair._id}
           key={idNamePair._id}
-          sx={{ marginTop: "10px", display: "flex", p: 0 }}
+          sx={{ display: "flex" }}
           button
           onClick={(event) => {
             handleLoadList(event, idNamePair._id);
@@ -68,44 +87,128 @@ function CommunityListCard(props) {
             width: "100%",
           }}
         >
-          <Box sx={{ p: 1, flexGrow: 1 }}>{idNamePair.name}</Box>
-          <Box>
+          <Box sx={{ p: 0, flexGrow: 1, fontSize: "25px" }}>
+            {idNamePair.name}
+          </Box>
+
+          <Box marginX={4.5}>
             <IconButton aria-label="dislikes">
               <ThumbUpIcon
+                style={{ fontSize: "30pt", marginInline: "10px" }}
                 onClick={(event) => {
-                  like(event, idNamePair._id);
+                  handlelike(event, idNamePair._id);
                 }}
               />{" "}
-              {idNamePair.likes}
+              {like}
             </IconButton>
           </Box>
-          <Box>
+          <Box marginX={4.5}>
             <IconButton aria-label="dislikes">
               <ThumbDownIcon
+                style={{ fontSize: "30pt", marginInline: "10px" }}
                 onClick={(event) => {
-                  dislike(event, idNamePair._id);
+                  handledislike(event, idNamePair._id);
                 }}
               />{" "}
-              {idNamePair.dislikes}
+              {dislike}
             </IconButton>
           </Box>
-          <ArrowDownwardIcon onClick={expand} />
-          <div>views: 32</div>
         </ListItem>
+      </Box>
+      <div style={{ marginLeft: "17px", paddingBottom: "10px" }}>
+        <Box>
+          By:
+          <Link
+            sx={{ color: "blue", textDecorationColor: "blue" }}
+            marginLeft={3}
+            fontSize={20}
+            href="#"
+          >
+            {auth.user.userName}
+          </Link>
+        </Box>
+
+        <div style={{ color: "#00FF00" }}>
+          <div style={{ float: "left", marginRight: "15px", color: "black" }}>
+            Published:{" "}
+          </div>
+          {dateFormat(idNamePair.createdAt, "mmmm d, yyyy")}
+          <div style={{ float: "right" }}>
+            <div
+              style={{
+                color: "black",
+                float: "left",
+                marginTop: "-25px",
+                marginRight: "175px",
+                fontSize: "14pt",
+              }}
+            >
+              Views:{" "}
+              <div style={{ color: "red", float: "right", marginLeft: "10px" }}>
+                32
+              </div>
+            </div>
+            <div
+              style={{
+                color: "black",
+                float: "right",
+                marginTop: "-35px",
+                marginRight: "25px",
+                marginLeft: "25px",
+              }}
+            >
+              <ArrowDownwardIcon
+                style={{ fontSize: "30pt" }}
+                onClick={expand}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div>By: {idNamePair.userName}</div>
-      <div>Published: {idNamePair.createdAt}</div>
     </div>
+  );
+
+  let Comments = (
+    <>
+      <Box
+        mr={3}
+        p={0.5}
+        sx={{
+          bgcolor: "#FF8000",
+          border: "1px solid black",
+          borderRadius: "15px",
+          marginBlock: "10px",
+          marginTop: 0,
+        }}
+      >
+        <Link
+          sx={{
+            color: "blue",
+            textDecorationColor: "blue",
+            marginLeft: "15px",
+          }}
+          fontSize={15}
+          href="#"
+        >
+          {auth.user.userName}
+        </Link>
+        <div style={{ marginLeft: "15px", marginTop: "10px" }}>COMMENT</div>
+      </Box>
+    </>
   );
 
   if (expanding) {
     cardElement = (
-      <div>
-        <div>
+      <div
+        style={{
+          margin: "15px",
+        }}
+      >
+        <Box>
           <ListItem
             id={idNamePair._id}
             key={idNamePair._id}
-            sx={{ marginTop: "10px", display: "flex", p: 0 }}
+            sx={{ display: "flex" }}
             button
             onClick={(event) => {
               handleLoadList(event, idNamePair._id);
@@ -115,22 +218,131 @@ function CommunityListCard(props) {
               width: "100%",
             }}
           >
-            <Box sx={{ p: 1, flexGrow: 1 }}>{idNamePair.name}</Box>
-            <ThumbUpIcon />
-            <ThumbDownIcon />
-            <ArrowUpwardIcon onClick={expand} />
-            <div>views: 32</div>
+            <Box sx={{ p: 0, flexGrow: 1, fontSize: "25px" }}>
+              {idNamePair.name}
+            </Box>
+
+            <Box marginX={4.5}>
+              <IconButton aria-label="dislikes">
+                <ThumbUpIcon
+                  style={{ fontSize: "30pt", marginInline: "10px" }}
+                  onClick={(event) => {
+                    like(event, idNamePair._id);
+                  }}
+                />{" "}
+                {idNamePair.likes}
+              </IconButton>
+            </Box>
+            <Box marginX={4.5}>
+              <IconButton aria-label="dislikes">
+                <ThumbDownIcon
+                  style={{ fontSize: "30pt", marginInline: "10px" }}
+                  onClick={(event) => {
+                    dislike(event, idNamePair._id);
+                  }}
+                />{" "}
+                {idNamePair.dislikes}
+              </IconButton>
+            </Box>
           </ListItem>
-          <Box sx={{ p: 1 }}>
-            <Box sx={{ p: 1 }}>1. {item1}</Box>
-            <Box sx={{ p: 1 }}>2. {item2}</Box>
-            <Box sx={{ p: 1 }}>3. {item3}</Box>
-            <Box sx={{ p: 1 }}>4. {item4}</Box>
-            <Box sx={{ p: 1 }}>5. {item5}</Box>
+        </Box>
+        <div style={{ marginLeft: "17px", paddingBottom: "10px" }}>
+          <Box>
+            By:
+            <Link
+              sx={{ color: "blue", textDecorationColor: "blue" }}
+              marginLeft={3}
+              fontSize={20}
+              href="#"
+            >
+              {auth.user.userName}
+            </Link>
           </Box>
+
+          <Box
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginBlock: "15px",
+            }}
+          >
+            <Box
+              sx={{
+                p: 2,
+                bgcolor: "#2C3287",
+                width: "45%",
+                borderRadius: 2,
+              }}
+            >
+              <Box sx={{ p: 1, fontSize: "30pt" }}>1. {item1}</Box>
+              <Box sx={{ p: 1, fontSize: "30pt" }}>2. {item2}</Box>
+              <Box sx={{ p: 1, fontSize: "30pt" }}>3. {item3}</Box>
+              <Box sx={{ p: 1, fontSize: "30pt" }}>4. {item4}</Box>
+              <Box sx={{ p: 1, fontSize: "30pt" }}>5. {item5}</Box>
+            </Box>
+
+            <Box height={315} sx={{ mt: -3, mr: 4, width: "45%" }}>
+              <Box
+                height={275}
+                sx={{
+                  fontSize: "16pt",
+                  overflowY: "scroll",
+                }}
+              >
+                {Comments}
+              </Box>
+              <TextField
+                sx={{
+                  width: "95%",
+                  bgcolor: "white",
+                  borderRadius: 1,
+                  marginTop: "10px",
+                }}
+                placeholder="Add Comment"
+              />
+            </Box>
+          </Box>
+
+          <div style={{ color: "#00FF00" }}>
+            <div style={{ float: "left", marginRight: "15px", color: "black" }}>
+              Published:{" "}
+            </div>
+            {dateFormat(idNamePair.createdAt, "mmmm d, yyyy")}
+            <div style={{ float: "right" }}>
+              <div
+                style={{
+                  color: "black",
+                  float: "left",
+                  marginTop: "0px",
+                  marginRight: "175px",
+                  fontSize: "14pt",
+                }}
+              >
+                Views:{" "}
+                <div
+                  style={{ color: "red", float: "right", marginLeft: "10px" }}
+                >
+                  32
+                </div>
+              </div>
+              <div
+                style={{
+                  color: "black",
+                  float: "right",
+                  marginTop: "-15px",
+                  marginRight: "25px",
+                  marginLeft: "25px",
+                }}
+              >
+                <ArrowUpwardIcon
+                  style={{ fontSize: "30pt" }}
+                  onClick={expand}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div>By: {auth.user.userName}</div>
-        <div>Published: {idNamePair.createdAt}</div>
       </div>
     );
   }
